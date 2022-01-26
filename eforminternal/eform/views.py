de59963990a2321f2425b65d72e456917ddbebee
roy.lo <roy0001919@gmail.com>
2022-01-26 13:52:45 +0800

    update

--- eforminternal/eform/views.py
+++ eforminternal/eform/views.py
@@ -11,17 +11,15 @@ scheduler = BackgroundScheduler()
 
 @login_required
 def tab_coform(request):
-    if request.method == 'POST':
-        print("123")
-    else:
-        datetime_dt = datetime.today()  # 獲得當地時間
-        order_id = datetime_dt.strftime("%Y%m%d%H%M%S")  # 格式化日期
 
-        return render(
-            request,
-            'myweb/tab_coform.html',
-            {"order_id": order_id},
-        )
+    datetime_dt = datetime.today()  # 獲得當地時間
+    order_id = datetime_dt.strftime("%Y%m%d%H%M%S")  # 格式化日期
+
+    return render(
+        request,
+        'myweb/tab_coform.html',
+        {"order_id": order_id},
+    )
 
 @login_required
 def tab_daily(request):
